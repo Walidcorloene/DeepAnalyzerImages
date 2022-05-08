e846340c226e56ff13cb9053c936ec797b9ab802
walid khirdine <khirdine.walid@gmail.com>
2022-05-08 15:45:01 +0200

    new images and transform scraper.py from iterative into functional

--- Scraper/scraper.py
+++ Scraper/scraper.py
@@ -1,7 +1,5 @@
 # import web driver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from urllib.error import URLError
 from selenium import webdriver
 from dotenv import load_dotenv
 from parsel import Selector
@@ -14,60 +12,65 @@ import os
 load_dotenv(verbose=True)
 
 # specifies the path to the chromedriver.exe
-driver = webdriver.Chrome(os.getenv("WEBDRIVER_PATH"))
 
-# Maximaze page size
-driver.maximize_window()
 
-# driver.get method() will navigate to a page given by the URL address
-driver.get('https://www.linkedin.com')
+def init_webdriver():
+    driver = webdriver.Chrome(os.getenv("WEBDRIVER_PATH"))
 
-# locate email form by_class_name
-username = driver.find_element(By.ID, 'session_key')
+    # Maximaze page size
+    driver.maximize_window()
+    # driver.get method() will navigate to a page given by the URL address
+    driver.get('https://www.linkedin.com')
 
-# send_keys() to simulate key strokes
-username.send_keys(os.getenv("LINKEDIN_USERNAME"))
+    # locate email form by_class_name
+    username = driver.find_element(By.ID, 'session_key')
 
-# sleep for 0.5 seconds
-time.sleep(0.5)
+    # send_keys() to simulate key strokes
+    username.send_keys(os.getenv("LINKEDIN_USERNAME"))
 
-# locate password form by_class_name
-password = driver.find_element(By.ID, 'session_password')
+    # sleep for 0.5 seconds
+    time.sleep(0.5)
 
-# send_keys() to simulate key strokes
-password.send_keys(os.getenv("LINKEDIN_PASSWORD"))
+    # locate password form by_class_name
+    password = driver.find_element(By.ID, 'session_password')
 
-time.sleep(0.5)
+    # send_keys() to simulate key strokes
+    password.send_keys(os.getenv("LINKEDIN_PASSWORD"))
 
-# locate submit button by_xpath
-sign_in_button = driver.find_element(By.XPATH, '//*[@type="submit"]')
+    time.sleep(0.5)
 
-# signin button click
-sign_in_button.click()
+    # locate submit button by_xpath
+    sign_in_button = driver.find_element(By.XPATH, '//*[@type="submit"]')
 
-time.sleep(0.5)
+    # signin button click
+    sign_in_button.click()
 
+    time.sleep(0.5)
+    return driver
 
 # get source code of my networks profiles and scroll n times
-def networkSource(link):
+
+
+def networkSource(link, driver):
     driver.get(link)
     time.sleep(3)
     # To close messaging list
-    MessagingList= driver.fin_element(By.ID,'ember77')
+    MessagingList = driver.find_element(By.ID, 'ember75')
     MessagingList.click()
     # Scroll 50 times to get 600 profiles
-    for _ in range(50):
+    for _ in range(40):
         # To scoll execute the scroll
         driver.execute_script(
             "window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(1.5)
-        # appending the source code of the page 
+        # appending the source code of the page
         sel = Selector(text=driver.page_source)
     return sel
 
 
 # get Href of each linkeidn profile in source code
-def LinkedinHref(sel=networkSource('https://www.linkedin.com/mynetwork/')):
+def LinkedinHref(driver):
+    sel = networkSource('https://www.linkedin.com/mynetwork/', driver)
     linkedin = []
     path = 'https://www.linkedin.com'
     # to get all the link in the script a
@@ -80,10 +83,11 @@ def LinkedinHref(sel=networkSource('https://www.linkedin.com/mynetwork/')):
     return linkedin_urls
 
 
-def downloadProfile():
+def downloadProfile(driver):
     # getting our list of images
     imagename = os.listdir(os.getenv("LOCAL_IMAGES"))
-    for linkedin_url in LinkedinHref():
+    for linkedin_url in LinkedinHref(driver):
+        #   print("one url: ", linkedin_url)
         # get the profile URL
         driver.get(linkedin_url)
         # Get picture URL
@@ -91,23 +95,28 @@ def downloadProfile():
             By.CLASS_NAME, 'pv-top-card__non-self-photo-wrapper.ml0')
         for url in picture_url:
             # get the source of the images profile
-            a = url.find_element(By.TAG_NAME, 'img').get_attribute('src')
-            # get the source of the name profile
-            text = driver.find_element(By.CLASS_NAME,
-                                       "text-heading-xlarge.inline.t-24.v-align-middle.break-words").text
-            # # download the image
-            try:
+            url_img = url.find_element(By.TAG_NAME, 'img').get_attribute('src')
+            if (str(url_img).startswith("https://media-exp1.licdn.com/")):
+                # get the source of the name profile
+                text = driver.find_element(
+                    By.CLASS_NAME, "text-heading-xlarge.inline.t-24.v-align-middle.break-words").text
+                # # download the image
                 # Compare the name of local images and the futur downloading images to not have a double
-                if text+'png' not in imagename:
-                    urllib.request.urlretrieve(a, "images/"+text+".png")
-                    # add a 5 second pause loading each URL
-                    time.sleep(5)
-            except URLError as e:
-                print(e)
-            finally:
-                print("Download successfull")
-                driver.close()
+                try:
+                    if text+'.png' not in imagename:
+                        urllib.request.urlretrieve(
+                            url_img, "images/"+text+".png")
+                        print("Download successfull")
+                        # add a 5 second pause loading each URL
+                        time.sleep(5)
+                except urllib.error.HTTPError as e:
+                    print(e.code)
+                    print(e.read())
+                    continue  # continue to next row
+    driver.close()
 
 
 if __name__ == "__main__":
-    downloadProfile()
+    driver = init_webdriver()
+
+    downloadProfile(driver)
